12c93fca475ca6d2a0a70e0e9248e5fa3eaf2ca0
notjohnryan <notryan58@gmail.com>
2026-01-15 19:29:45 +0800

    Change payment method types to 'qrph'

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/pay', async (req, res) => {
         data: {
           attributes: {
             line_items: [{ amount: price, currency: 'PHP', name: "Token Pack", quantity: qty }],
-            payment_method_types: ['gcash', 'card', 'paymaya'],
+            payment_method_types: ['qrph'],
             success_url: process.env.SUCCESS_URL,
             metadata: { userId, token_credits: (qty * 5000000).toString() }
           }
