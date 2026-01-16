0016892ecf17bb22fe7d734461a1b5bf23e0594e
notjohnryan <notryan58@gmail.com>
2026-01-16 18:33:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ app.get('/pay', async (req, res) => {
             send_email_receipt: true, 
             billing: { email: email },
             show_description: true,
-            customer_email: email
-            billing: { email: email },
             line_items: [{ 
               amount: price, 
               currency: 'PHP', 
